baf6356083088a08f9bb2d0152c243a4fb655490
kimgusxo <81300338+kimgusxo@users.noreply.github.com>
2025-01-30 15:39:01 +0900

    [Silver I] Title: 문자열 교환, Time: 104 ms, Memory: 14252 KB -BaekjoonHub

---
+++ 백준/Silver/1522. 문자열 교환/문자열 교환.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String line = br.readLine();
+
+        int cnt = 0;
+
+        for (int i = 0; i < line.length(); i++) {
+            if (line.charAt(i) == 'a') {
+                cnt++;
+            }
+        }
+
+        line += line;
+        int min = 1000;
+
+        for(int i = 0; i < line.length() - cnt; i++) {
+            String sub = line.substring(i, i+cnt);
+            int swap = 0;
+
+            for (int j = 0; j < sub.length(); j++) {
+                if (sub.charAt(j) == 'b') {
+                    swap++;
+                }
+            }
+
+            if(min > swap) {
+                min = swap;
+            }
+        }
+
+        System.out.println(min);
+    }
+}
