5fcc3998c6c4d1c257a8973b7343c02052e39147
kimgusxo <81300338+kimgusxo@users.noreply.github.com>
2025-02-05 04:13:59 +0900

    [Silver IV] Title: 접미사 배열, Time: 172 ms, Memory: 16008 KB -BaekjoonHub

---
+++ 백준/Silver/11656. 접미사 배열/접미사 배열.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+
+        ArrayList<String> pq = new ArrayList<>();
+
+        for(int i = 0; i < str.length(); i++) {
+            pq.add(str.substring(i));
+        }
+
+        Collections.sort(pq);
+
+        pq.forEach(System.out::println);
+
+    }
+}
