4a1a1a0bed23cf0f0db6079e655b026b54569cc5
kimgusxo <81300338+kimgusxo@users.noreply.github.com>
2025-02-22 15:40:42 +0900

    [Silver IV] Title: 큐, Time: 296 ms, Memory: 19748 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.java
@@ -0,0 +1,65 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        Deque<Integer> deque = new ArrayDeque<>();
+
+        for(int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            switch(st.nextToken()) {
+                case "push": {
+                    deque.offer(Integer.parseInt(st.nextToken()));
+                    break;
+                }
+                case "pop": {
+                    if(deque.isEmpty()) {
+                        System.out.println(-1);
+                    } else {
+                        System.out.println(deque.pollFirst());
+                    }
+                    break;
+                }
+                case "size": {
+                    System.out.println(deque.size());
+                    break;
+                }
+                case "empty": {
+                    if(deque.isEmpty()) {
+                        System.out.println(1);
+                    } else {
+                        System.out.println(0);
+                    }
+                    break;
+                }
+                case "front": {
+                    if(deque.isEmpty()) {
+                        System.out.println(-1);
+                    } else {
+                        System.out.println(deque.peekFirst());
+                    }
+                    break;
+                }
+                case "back": {
+                    if(deque.isEmpty()) {
+                        System.out.println(-1);
+                    } else {
+                        System.out.println(deque.peekLast());
+                    }
+                    break;
+                }
+
+            }
+        }
+
+    }
+}
