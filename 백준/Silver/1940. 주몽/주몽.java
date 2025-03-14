5e21244bea76e436967fe951832673e4784be63c
kimgusxo <81300338+kimgusxo@users.noreply.github.com>
2025-03-15 04:47:38 +0900

    [Silver IV] Title: 주몽, Time: 352 ms, Memory: 17208 KB -BaekjoonHub

---
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int cnt = 0;
+
+        for(int i = 0; i < N; i++) {
+            int n1 = arr[i];
+            for(int j = i+1; j < N; j++) {
+                int n2 = arr[j];
+
+                if(n1+n2 == M) {
+                    cnt++;
+                }
+            }
+        }
+
+        System.out.println(cnt);
+    }
+}
