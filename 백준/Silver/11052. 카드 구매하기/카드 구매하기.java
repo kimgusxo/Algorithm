eea4fc9b365f8d046bf3f12796990b9bd4d5f35c
김현태 <gusxoas9906@kumoh.ac.kr>
2025-05-23 18:57:56 +0900

    [Silver I] Title: 카드 구매하기, Time: 152 ms, Memory: 14544 KB -BaekjoonHub

---
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int[] num = new int[N+1];
+        for (int i = 1; i <= N; i++) {
+            num[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] dp = new int[N+1];
+        dp[1] = num[1];
+        for (int i = 2; i <= N; i++) {
+            dp[i] = num[i];
+            for(int j = 1; j < i; j++) {
+                dp[i] = Math.max(dp[i], dp[j]+dp[i-j]);
+            }
+        }
+
+        System.out.println(dp[N]);
+    }
+}
+
+
+
+
