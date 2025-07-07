d69462b6eadce18d990245436e65f5fdd50cd38e
김현태 <gusxoas9906@kumoh.ac.kr>
2025-07-07 23:41:58 +0900

    [level 2] Title: 2 x n 타일링, Time: 2.63 ms, Memory: 54 MB -BaekjoonHub

---
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.java
@@ -0,0 +1,19 @@
+class Solution {
+    
+    static final int MOD = 1_000_000_007;
+    
+    public int solution(int n) {
+        int answer = 0;
+        
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i = 2; i <= n; i++) {
+            dp[i] = (dp[i-1] + dp[i-2])%MOD;
+        }
+        
+        answer = dp[n];
+        return answer;
+    }
+    
+}
