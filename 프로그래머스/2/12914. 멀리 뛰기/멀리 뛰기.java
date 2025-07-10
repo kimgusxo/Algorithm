8501eb42f98e90e21ce8cc40cc280239ad31341a
김현태 <gusxoas9906@kumoh.ac.kr>
2025-07-10 18:31:56 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.13 ms, Memory: 72.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -0,0 +1,20 @@
+class Solution {
+
+    static final int MOD = 1_234_567;
+    
+    public long solution(int n) {
+        long answer = 0;
+
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for(int i = 2; i <= n; i++) {
+            dp[i] = (dp[i-1]+dp[i-2])%MOD;
+        }
+
+        answer = dp[n];
+
+        return answer;
+    }
+}
