8a98205eb59595d70b1d090c2985af01ce3ca747
김현태 <gusxoas9906@kumoh.ac.kr>
2025-07-18 09:12:11 +0900

    [level 0] Title: 팩토리얼, Time: 0.02 ms, Memory: 72.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120848. 팩토리얼/팩토리얼.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 1;
+        for(int i = 1; i <= 10; i++) {
+            answer *= i;
+            if(answer > n) {
+                answer = i-1;
+                break;  
+            } else if(answer == n) {
+                answer = i;
+                break;
+            }
+        }
+        return answer;
+    }
+}
