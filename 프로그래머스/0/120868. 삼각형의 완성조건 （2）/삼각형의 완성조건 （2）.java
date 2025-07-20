acede7bf2e0d22bc5386ee7cfa769cc25bb263d1
김현태 <gusxoas9906@kumoh.ac.kr>
2025-07-20 16:01:39 +0900

    [level 0] Title: 삼각형의 완성조건 (2), Time: 0.04 ms, Memory: 70.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(int[] sides) {
+        int answer = 0;
+        int max = Math.max(sides[0], sides[1]);
+        answer = max-Math.abs(sides[0]-sides[1]) + (sides[0]+sides[1])-max-1; 
+        return answer;
+    }
+}
