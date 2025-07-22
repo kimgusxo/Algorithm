ffba579c6535d68ffa9f07a92911b86e06591cdb
김현태 <gusxoas9906@kumoh.ac.kr>
2025-07-22 14:29:09 +0900

    [level 0] Title: 약수 구하기, Time: 4.74 ms, Memory: 89.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
@@ -0,0 +1,8 @@
+import java.util.*;
+import java.util.stream.IntStream;
+
+class Solution {
+    public int[] solution(int n) {
+        return IntStream.range(1, n+1).filter(x -> n%x == 0).toArray();
+    }
+}
