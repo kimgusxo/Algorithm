8b3b7dc6eacdfac6dd3d1d4ac3a6327cd37a0775
김현태 <gusxoas9906@kumoh.ac.kr>
2025-08-16 18:11:36 +0900

    [level 0] Title: A 강조하기, Time: 0.09 ms, Memory: 79.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.java
@@ -0,0 +1,8 @@
+import java.util.*;
+import java.util.stream.*;
+
+class Solution {
+    public String solution(String myString) {
+            return myString.toLowerCase().replaceAll("a", "A");
+    }
+}
