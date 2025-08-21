c603796abd95f2410b09d0acd5fafcdcf2cf825c
김현태 <gusxoas9906@kumoh.ac.kr>
2025-08-22 04:06:15 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.10 ms, Memory: 85.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+
+        for(char c : String.valueOf(n).toCharArray()) {
+            answer += Character.getNumericValue(c);
+        }
+        
+        return answer;
+    }
+}
