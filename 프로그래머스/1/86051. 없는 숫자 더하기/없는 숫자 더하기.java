fee122cc8411ca76584eea32f2e45105c896978a
김현태 <gusxoas9906@kumoh.ac.kr>
2025-08-24 17:18:13 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.07 ms, Memory: 86.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        
+        Set<Integer> set = new HashSet<>();
+        for(int el : numbers) {
+            set.add(el);
+        }
+        
+        for(int i = 0; i < 10; i++) {
+            if(!set.contains(i)) answer += i;
+        }
+        
+        return answer;
+    }
+}
