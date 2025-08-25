a2f8acf41ec22f6f323be9e1268f583452fa6aa7
김현태 <gusxoas9906@kumoh.ac.kr>
2025-08-25 19:06:29 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 7.81 ms, Memory: 77 MB -BaekjoonHub

---
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] seoul) {
+        List<String> answer = Arrays.asList(seoul);
+        return "김서방은 " + answer.indexOf("Kim") + "에 있다";
+    }
+}
