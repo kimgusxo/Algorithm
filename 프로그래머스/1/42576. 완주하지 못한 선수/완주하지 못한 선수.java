dfeb08412bbb5f34e783b0c61ceede78c6753c6f
김현태 <gusxoas9906@kumoh.ac.kr>
2025-09-16 00:57:06 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 370.06 ms, Memory: 96.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+        
+        for(int i = 0; i < completion.length; i++) {
+            if(!participant[i].equals(completion[i])) {
+                answer = participant[i];
+                break;
+            }
+        }
+        
+        return answer.isEmpty() ? participant[participant.length-1] : answer;
+    }
+}
