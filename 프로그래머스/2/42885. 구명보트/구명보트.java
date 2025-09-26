89a546518b75750d862e2d592b2bb67d0fe9b388
김현태 <gusxoas9906@kumoh.ac.kr>
2025-09-26 22:11:04 +0900

    [level 2] Title: 구명보트, Time: 12.32 ms, Memory: 57.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.util.stream.*;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+
+        Arrays.sort(people);
+
+        int start = 0;
+        int end = people.length-1;
+
+        while(start <= end) {
+            if (people[start] + people[end] <= limit) {
+                start++;
+            }
+            end--;
+            answer++;
+        }
+
+        return answer;
+    }
+}
