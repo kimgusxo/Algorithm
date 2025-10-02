57781f23de1f418ca93588fbb257a8dec987c4e1
김현태 <gusxoas9906@kumoh.ac.kr>
2025-10-02 19:15:15 +0900

    [level 2] Title: 튜플, Time: 107.68 ms, Memory: 111 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -2,11 +2,6 @@ import java.util.*;
 
 class Solution {
     
-    // 일단 replace를 통해 괄호 없애고 split(",") 한 뒤 숫자원소 넣기
-    // 맵만들고 <값, 빈도수>로 넣기
-    // 맵을 밸류값으로 정렬 후 리스트에 넣기
-    // 리스트 배열화
-    
     public int[] solution(String s) {
         Map<String, Integer> map = new HashMap<>();
         
